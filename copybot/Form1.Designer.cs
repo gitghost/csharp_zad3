d3b895ce9ea6b8daf13eafbc7fb3fee7cb6a855f
gitghost <cssghost@wp.pl>
2019-04-10 18:35:10 +0200

    ZAD3

---
+++ copybot/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace copybot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "wybierz plik";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Przeglądaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "nazwa pliku";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "- - -";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "liczba projektów";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(260, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(260, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(126, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "liczba plików";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(209, 254);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 350);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ copybot/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace copybot
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();         
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "sln files (*.sln)|*.sln|All files (*.*)|*.*";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                var sol = new Solution(file);
+                label5.Text = sol.Projects.Count.ToString();
+
+                var mojxml = new XmlDocument();
+                try
+                {
+                    int project_nr = 0;
+                    int files_count=0;
+
+                    foreach (SolutionProject temp in sol.Projects)
+                    {
+                        string directoryPath = Path.GetDirectoryName(openFileDialog1.FileName);
+                        //Console.WriteLine(directoryPath+"\\" + sol.Projects[project_nr].ProjectName + "\\" +sol.Projects[project_nr].ProjectName+".csproj");
+            
+                        mojxml.Load(directoryPath + "\\" + sol.Projects[project_nr].ProjectName + "\\" + sol.Projects[project_nr].ProjectName + ".csproj");
+
+                        XmlNode currNode = mojxml.DocumentElement.FirstChild;
+                        do
+                        {
+                            currNode = currNode.NextSibling;
+                        } while (currNode.Name != "ItemGroup");
+                        currNode = currNode.NextSibling;
+                        currNode = currNode.FirstChild;
+
+                        var lista = new List<string>();
+                        XmlElement jeden = (XmlElement)currNode;
+                        XmlAttribute attr = jeden.GetAttributeNode("Include");
+                        lista.Add(attr.InnerXml);
+                        //Console.WriteLine(attr.InnerXml);
+
+                        bool warunek = true;
+                        while(warunek)          //STRASZNIE ŁOPATOLOGICZNIE
+                        {                      
+                            if (currNode.NextSibling != null)
+                            {
+                                currNode = currNode.NextSibling;
+                                jeden = (XmlElement)currNode;
+                                attr = jeden.GetAttributeNode("Include");
+                                //Console.WriteLine(attr.InnerXml);
+                                lista.Add(attr.InnerXml);
+                            }
+                            else { warunek = false; }  
+                        }
+                        
+
+                        string sourcePath = Path.GetFullPath(@"..\..\..\");
+                        string destinationPath = Path.GetFullPath(@"..\..\..\KOPIA "+ sol.Projects[project_nr].ProjectName);
+
+                        
+                        string sourceFileName = openFileDialog1.SafeFileName;                      
+                        string destinationFileName = "COPY " + sourceFileName;
+                        string sourceFile = Path.Combine(sourcePath, sourceFileName);
+                        string destinationFile = Path.Combine(destinationPath, destinationFileName);
+
+      
+                        if (!Directory.Exists(destinationPath))
+                        {
+                            Directory.CreateDirectory(destinationPath);
+                        }
+                        File.Copy(sourceFile, destinationFile, true);       //kopiuje SLN RECZNIE
+                                                                            //przy wielu projektach bedzie sie kopiował wielokrotnie
+                                                                            //do każdego folderu z plikami projektu
+                        sourcePath = directoryPath+ "\\" + sol.Projects[project_nr].ProjectName;
+                        Console.WriteLine("PATH: " + sourcePath);
+                        sourceFileName = sol.Projects[project_nr].ProjectName + ".csproj";
+                        Console.WriteLine("CSPROJ: " + sourceFileName);
+                        destinationFileName = "COPY " + sourceFileName;
+                        sourceFile = Path.Combine(sourcePath, sourceFileName);
+                        Console.WriteLine("sourceFile: "+sourceFile);
+                        destinationFile = Path.Combine(destinationPath, destinationFileName);
+                        File.Copy(sourceFile, destinationFile, true);       //kopiuje CSPROJ
+
+                        foreach (string plik in lista)
+                        {
+                            string[] name_order;
+                            if (plik.Contains("\\"))
+                            {
+                                name_order = plik.Split('\\');
+                                sourceFileName = name_order[1];                                                               
+                                sourceFile = Path.Combine(sourcePath+"\\"+name_order[0], sourceFileName);
+                                Console.WriteLine("SOURCE FILE: " + sourceFile);
+                                destinationFileName = "COPY " + sourceFileName;
+                                destinationPath = Path.GetFullPath(@"..\..\..\KOPIA " + sol.Projects[project_nr].ProjectName+"\\"+name_order[0]);
+                                destinationFile = Path.Combine(destinationPath, destinationFileName);
+                            }
+                            else
+                            {
+                                sourceFileName = plik;
+                                sourceFile = Path.Combine(sourcePath, sourceFileName);
+                                destinationFileName = "COPY " + sourceFileName;
+                                destinationPath = Path.GetFullPath(@"..\..\..\KOPIA " + sol.Projects[project_nr].ProjectName);
+                                destinationFile = Path.Combine(destinationPath, destinationFileName);
+                            }
+                            
+                            if (!Directory.Exists(destinationPath))
+                            {
+                                Directory.CreateDirectory(destinationPath);
+                            }
+                            File.Copy(sourceFile, destinationFile, true); 
+                        }                                                   
+                        project_nr++;
+                        files_count += lista.Count+2; // liczba plików projeku + projekt + solucja
+                        label6.Text = files_count.ToString();
+                        label8.Text = "SKOPIOWANO PLIKI";
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    label6.Text = "BRAK PLIKU";
+                }
+                try
+                {
+                    label3.Text = openFileDialog1.SafeFileName;
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "KOPIOINATOR";
+        }
+    }
+}
